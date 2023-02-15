0006eecd6561da3641a4959216747c5bee6339ad
gharjare <gharjarechetan@gmail.com>
2023-02-15 16:32:08 +0530

    [chetan gharjare] create binary search tree

--- Program.cs
+++ Program.cs
@@ -5,8 +5,11 @@
        public static void Main(string[] args)
        {
             Console.WriteLine("Welcome to Binary search treee");
-
-       }
+             BinaryTree<int> tree = new BinaryTree<int>(56);
+            tree.AddNode(30);
+            tree.AddNode(70);
+            tree.Display();
+        }
     }
 
 }
